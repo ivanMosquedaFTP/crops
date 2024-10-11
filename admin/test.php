27a238efbfc0d5a53bb45bc132cc49d5a9f7af7e
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-10 19:19:23 -0600

    TODO

---
+++ admin/test.php
@@ -0,0 +1,7 @@
+<?php
+    require_once('../sistema.class.php');
+    $app = new sistema;
+    $roles = $app->getRole('luislao@itcelaya.edu.mx');
+    print_r($roles);
+    $privilegio=$app->getPrivilegios('l21031178@celaya.tecnm.mx');
+?>
--- sistema.class.php
+++ sistema.class.php
@@ -9,5 +9,41 @@
         function alerta($tipo, $mensaje) {
             include('views/alert.php');
         }
+
+        function getRole($correo) {
+            $this -> conexion();
+            $data = [];
+            if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+                $sql = "select r.rol from usuario u inner join usuario_rol ur on u.id_usuario = ur.id_usuario
+                    inner JOIN rol r on r.id_rol = ur.id_rol
+                    where u.correo = :correo ";
+
+                $roles = $this->conn->prepare $sql;
+                $roles->bindParam(":correo",$correo,PDO::PARAM_STR);
+                $roles->execute();
+                $data = $roles->fetchAll(PDO::FETCH_ASSOC);
+            }
+            
+            return $data;
+        }
+
+        function getPrivilegios($correo) {
+            $this -> conexion();
+            $data = [];
+            if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+                $sql = "select p.permiso from usuario u inner join usuario_rol ur on u.id_usuario = ur.id_usuario
+                        inner JOIN rol r on r.id_rol = ur.id_rol
+                        INNER JOIN rol_permiso rp on rp.id_rol = r.id_rol
+                        inner JOIN permiso p on p.id_permiso = rp.id_permiso
+                            where u.correo = :correo;";
+
+                $privilegio = $this->conn->prepare $sql;
+                $privilegio->bindParam(":correo",$correo,PDO::PARAM_STR);
+                $privilegio->execute();
+                $data = $privilegio->fetchAll(PDO::FETCH_ASSOC);
+            }
+            
+            return $data;
+        }
     }
 ?>
