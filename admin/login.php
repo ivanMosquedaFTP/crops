7edf16412e623b63afb4fc33e5926403f1d01886
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-15 11:36:30 -0600

    ci

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,19 @@
 <?php
   require_once('../sistema.class.php');
   include('views/login/index.php');
+
+  $app = new sistema;
+  $accion = (isset($_GET['accion']))?$_GET['accion'] : NULL;
+
+  switch ($accion) {
+    case 'login': {
+      print_r($_POST);
+      break;
+    }
+    
+    default: {
+      include('views/login/index.php');
+      break;
+    }
+  }
 ?>
