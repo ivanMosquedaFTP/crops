be5306b1d6c039bd7e1d03e00f05efd88743442a
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-17 11:35:40 -0600

    added headers

--- admin/views/invernadero/index.php
+++ admin/views/invernadero/index.php
@@ -1,4 +1,4 @@
-<?php require('views/header.php')?>
+<?php require('views/header/headerAdministrador.php')?>
   <h1>Invernaderos</h1>
   <?php if (isset($mensaje)): $app -> alerta($tipo, $mensaje); endif;?>
   <a href="invernadero.php?accion=crear" class="btn btn-success">Nuevo</a>
--- admin/views/seccion/index.php
+++ admin/views/seccion/index.php
@@ -1,4 +1,4 @@
-<?php require('views/header.php')?>
+<?php require('views/header/headerAdministrador.php')?>
   <h1>Secciones</h1>
   <?php if (isset($mensaje)): $app -> alerta($tipo, $mensaje); endif;?>
   <a href="seccion.php?accion=crear" class="btn btn-success">Nuevo</a>
