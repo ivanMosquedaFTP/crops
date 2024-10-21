c3e1de12864f6348fc6139a5e0efe66d9a66f3c6
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-21 15:15:29 -0600

    refactored usuario.class.php

---
+++ admin/usuario.class.php
@@ -0,0 +1,67 @@
+<?php
+ require_once ('../sistema.class.php');
+
+ class usuario extends sistema {
+    function create ($data){
+        $result = [];
+        $insertar = [];
+        $this -> conexion();
+        $sql="insert into usuario(correo, contrasena) values(:correo, :contrasena);";
+        $insertar = $this->con->prepare($sql);
+        $insertar -> bindParam(':correo', $data['correo'], PDO::PARAM_INT);
+        $insertar -> bindParam(':contrasena', $data['contrasena'], PDO::PARAM_STR);
+        /*$insertar -> bindParam(':id_invernadero', $data['id_invernadero'], PDO::PARAM_INT);*/
+        $insertar -> execute();
+        $result = $insertar -> rowCount();
+        return $result;
+    }
+
+    function update ($id, $data){
+        $this->conexion();
+        $result = [];
+        $sql = 'update usuario set correo=:correo, contrasena=:contrasena where id_usuario=:id_usuario;';
+        $modificar=$this->con->prepare($sql);
+        $modificar->bindParam(':correo',$data['correo'], PDO::PARAM_STR);
+        $modificar->bindParam(':contrasena',$data['contrasena'], PDO::PARAM_INT);
+        $modificar->bindParam(':id_usuario',$id, PDO::PARAM_INT);
+        $modificar->execute();
+        $result= $modificar->rowCount();
+        return $result;
+    }
+
+    function delete ($id){
+        $this -> conexion();
+        $result = [];
+        if (is_numeric($id)) {
+            $sql = "delete from usuario where id_usuario=:id_usuario;";
+            $eliminar = $this->con->prepare($sql);
+            $eliminar -> bindParam(':id_usuario', $id, PDO::PARAM_INT);
+            $eliminar -> execute();
+            $result = $eliminar -> rowCount();
+        }
+        return $result;
+    }
+
+    function readOne ($id){
+        $this->conexion();
+        $result = [];
+        $consulta = 'SELECT * FROM usuario where id_usuario=:id_usuario;';
+        $sql = $this->con->prepare($consulta);
+        $sql->bindParam(":id_usuario",$id,PDO::PARAM_INT);
+        $sql -> execute();
+
+        $result = $sql->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    function readAll (){
+        $this -> conexion();
+        $result = [];
+        $consulta ='select * from usuario;';
+        $sql = $this->con->prepare ($consulta); 
+        $sql -> execute();
+        $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
+        return $result;
+    }
+ }
+?>
