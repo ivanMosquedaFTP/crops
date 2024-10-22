f570cd3bac56f291a593873e02800b54768126c9
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-21 22:42:50 -0600

    refactored views for permiso crud

--- admin/views/permiso/index.php
+++ admin/views/permiso/index.php
@@ -1,32 +1,23 @@
 <?php require('views/header/headerAdministrador.php');?>
-  <h1>Invernaderos</h1>
+  <h1>Permisos</h1>
   <?php if (isset($mensaje)): $app -> alerta($tipo, $mensaje); endif;?>
-  <a href="invernadero.php?accion=crear" class="btn btn-success">Nuevo</a>
+  <a href="permiso.php?accion=crear" class="btn btn-success">Nuevo</a>
   <table class="table">
   <thead>
     <tr>
-      <th scope="col">Id invernadero</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Area</th>
-      <th scope="col">Latitud</th>
-      <th scope="col">Longitud</th>
-      <th scope="col">Fecha de creacion</th>
-      <th scope="col">Opciones</th>
+      <th scope="col">Id permiso</th>
+      <th scope="col">Permiso</th>
     </tr>
   </thead>
   <tbody>
-    <?php foreach($invernaderos as $invernadero): ?>
+    <?php foreach($permisos as $permiso): ?>
     <tr>
-      <th scope="row"><?php echo $invernadero ['id_invernadero']; ?></th>
-      <td><?php echo $invernadero ['invernadero']; ?></td>
-      <td><?php echo $invernadero ['area']; ?></td>
-      <td><?php echo $invernadero ['latitud']; ?></td>
-      <td><?php echo $invernadero ['longitud']; ?></td>
-      <td><?php echo $invernadero ['fecha_creacion']; ?></td>
+      <th scope="row"><?php echo $permiso ['id_permiso']; ?></th>
+      <td><?php echo $permiso ['permiso']; ?></td>
       <td>
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <a href="invernadero.php?accion=actualizar&id=<?php echo $invernadero ['id_invernadero']; ?>" class="btn btn-warning">Actualizar</a>
-          <a href="invernadero.php?accion=eliminar&id=<?php echo $invernadero ['id_invernadero']; ?>" class="btn btn-danger">Eliminar</a>
+          <a href="permiso.php?accion=actualizar&id=<?php echo $permiso ['id_permiso']; ?>" class="btn btn-warning">Actualizar</a>
+          <a href="permiso.php?accion=eliminar&id=<?php echo $permiso ['id_permiso']; ?>" class="btn btn-danger">Eliminar</a>
         </div>
       </td>
     </tr>
