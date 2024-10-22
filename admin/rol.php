ba445f39133f254ce1e05818fdae28bc9105262e
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-21 18:27:38 -0600

    refactored rol.php

--- admin/rol.php
+++ admin/rol.php
@@ -1,13 +1,13 @@
 <?php
-require_once ('invernadero.class.php');
-$app = new invernadero();
+require_once ('rol.class.php');
+$app = new rol();
 $app -> checkRole('administrador');
 
 $accion = (isset($_GET['accion']))?$_GET['accion'] : NULL;
 $id=(isset($_GET['id']))?$_GET['id']:null;
 switch ($accion) {
     case 'crear': {
-        include 'views/invernadero/crear.php';
+        include 'views/rol/crear.php';
         break;
     }
 
@@ -15,21 +15,21 @@ switch ($accion) {
         $data=$_POST['data'];
         $resultado = $app->create($data);
         if ($resultado) {
-            $mensaje = "Invernadero dado de alta correctamente";
+            $mensaje = "rol dado de alta correctamente";
             $tipo = "success";
         } else {
-            $mensaje = "El invernadero no ha sido dado de alta";
+            $mensaje = "El rol no ha sido dado de alta";
             $tipo = "danger";
         }
 
-        $invernaderos = $app->readAll();
-        include('views/invernadero/index.php');
+        $roles = $app->readAll();
+        include('views/rol/index.php');
         break;
     }
 
     case 'actualizar': {
-        $invernaderos = $app -> readOne($id); 
-        include('views/invernadero/crear.php');
+        $roles = $app -> readOne($id); 
+        include('views/rol/crear.php');
         break;
     }
     
@@ -37,14 +37,14 @@ switch ($accion) {
         $data= $_POST['data'];
         $result=$app->update($id,$data);
         if($result){
-            $mensaje="El invernadero se ha actualizado";
+            $mensaje="El rol se ha actualizado";
             $tipo="success";
         }else{
             $mensaje="No se ha actualizado";
             $tipo="danger";
         }
-        $invernaderos = $app->readAll();
-        include('views/invernadero/index.php');
+        $roles = $app->readAll();
+        include('views/rol/index.php');
         break;
     }
 
@@ -53,22 +53,22 @@ switch ($accion) {
             if (is_numeric($id)) {
                 $resultado = $app -> delete($id);
                 if ($resultado) {
-                    $mensaje = "El invernadero se elimino correctamente";
+                    $mensaje = "El rol se elimino correctamente";
                     $tipo = "success";
                 } else {
-                    $mensaje = "El invernadero no se elimino correctamente";
+                    $mensaje = "El rol no se elimino correctamente";
                     $tipo = "danger";
                 }
             }
         }
-        $invernaderos = $app->readAll();
-        include('views/invernadero/index.php');
+        $roles = $app->readAll();
+        include('views/rol/index.php');
         break;
     }
 
     default: {
-        $invernaderos = $app->readAll();
-        include 'views/invernadero/index.php';
+        $roles = $app->readAll();
+        include 'views/rol/index.php';
         break;
     }
 }
