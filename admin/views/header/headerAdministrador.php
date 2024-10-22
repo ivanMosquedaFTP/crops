feaf6b09eccd536f23d3dd41e2e00dee200504b2
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-21 22:11:06 -0600

    fixed typo at headerAdministrador

--- admin/views/header/headerAdministrador.php
+++ admin/views/header/headerAdministrador.php
@@ -24,9 +24,9 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownUsuario" role="button" data-bs-toggle="dropdown" aria-expanded="false">Usuario</a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownUsuario">
-            <li><a class="dropdown-item" href="usuario.php">Usuario</a></li>
-            <li><a class="dropdown-item" href="roles.php">Roles</a></li>
-            <li><a class="dropdown-item" href="permisos.php">Permisos</a></li>
+            <li><a class="dropdown-item" href="usuario.php">Usuarios</a></li>
+            <li><a class="dropdown-item" href="rol.php">Roles</a></li>
+            <li><a class="dropdown-item" href="permiso.php">Permisos</a></li>
           </ul>
         </li>
       </ul>
