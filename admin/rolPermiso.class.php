c440139121075de3200d70d640a95e897d2ea0c3
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-23 07:32:04 -0600

    TODO

--- admin/rolPermiso.class.php
+++ admin/rolPermiso.class.php
@@ -1,30 +1,28 @@
 <?php
  require_once ('../sistema.class.php');
 
- class Invernadero extends sistema {
+ class rolPermiso extends sistema {
     function create ($data){
         $result = [];
         $insertar = [];
         $this -> conexion();
-        $sql="insert into invernadero(area, fecha_creacion, invernadero, latitud, longitud) values(:area, :fecha_creacion, :invernadero, :latitud, :longitud);";
+        $sql="insert into rol_permiso(id_rol, id_permiso) values(:id_rol, :id_permiso);";
         $insertar = $this->con->prepare($sql);
-        $insertar -> bindParam(':area', $data['area'], PDO::PARAM_INT);
-        $insertar -> bindParam(':fecha_creacion', $data['fecha_creacion'], PDO::PARAM_STR);
-        $insertar -> bindParam(':invernadero', $data['invernadero'], PDO::PARAM_STR);
-        $insertar -> bindParam(':latitud', $data['latitud'], PDO::PARAM_STR);
-        $insertar -> bindParam(':longitud', $data['longitud'], PDO::PARAM_STR);
+        $insertar -> bindParam(':id_rol', $data['id_rol'], PDO::PARAM_INT);
+        $insertar -> bindParam(':id_permiso', $data['id_permiso'], PDO::PARAM_INT);
         $insertar -> execute();
         $result = $insertar -> rowCount();
         return $result;
     }
 
+    /*TODO:fix the query for this method*/
     function update ($id, $data){
         $this->conexion();
         $result = [];
-        $sql = 'update invernadero set invernadero=:invernadero, latitud=:latitud,longitud=:longitud,area=:area, fecha_creacion=:fecha_creacion where id_invernadero=:id_invernadero;';
+        $sql = 'update rol_permiso set id_rol=:id_rol, id_permiso=:id_permiso where id_rolPermiso=:id_rolPermiso;';
         $modificar=$this->con->prepare($sql);
-        $modificar->bindParam(':id_invernadero',$id, PDO::PARAM_INT);
-        $modificar->bindParam(':invernadero',$data['invernadero'], PDO::PARAM_STR);
+        $modificar->bindParam(':id_rolPermiso',$id, PDO::PARAM_INT);
+        $modificar->bindParam(':rolPermiso',$data['rolPermiso'], PDO::PARAM_STR);
         $modificar->bindParam(':longitud',$data['longitud'], PDO::PARAM_STR);
         $modificar->bindParam(':latitud',$data['latitud'], PDO::PARAM_STR);
         $modificar->bindParam(':area',$data['area'], PDO::PARAM_INT);
@@ -34,23 +32,25 @@
         return $result;
     }
 
+    /*TODO:fix the query for this method*/
     function delete ($id){
         $this -> conexion();
         $result = [];
-        $sql = "delete from invernadero where id_invernadero=:id_invernadero;";
+        $sql = "delete from rol_permiso where id_rolPermiso=:id_rolPermiso;";
         $eliminar = $this->con->prepare($sql);
-        $eliminar -> bindParam(':id_invernadero', $id, PDO::PARAM_INT);
+        $eliminar -> bindParam(':id_rolPermiso', $id, PDO::PARAM_INT);
         $eliminar -> execute();
         $result = $eliminar -> rowCount();
         return $result;
     }
 
+    /*TODO:fix the query for this method*/
     function readOne ($id){
         $this->conexion();
         $result = [];
-        $consulta = 'SELECT * FROM invernadero where id_invernadero=:id_invernadero;';
+        $consulta = 'SELECT * FROM rolPermiso where id_rolPermiso=:id_rolPermiso;';
         $sql = $this->con->prepare($consulta);
-        $sql->bindParam(":id_invernadero",$id,PDO::PARAM_INT);
+        $sql->bindParam(":id_rolPermiso",$id,PDO::PARAM_INT);
         $sql -> execute();
 
         $result = $sql->fetch(PDO::FETCH_ASSOC);
@@ -60,7 +60,7 @@
     function readAll (){
         $this -> conexion();
         $result = [];
-        $consulta ='select * from invernadero';
+        $consulta ='select * from rol_permiso';
         $sql = $this->con->prepare ($consulta); 
         $sql -> execute();
         $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
